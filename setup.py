f97692082fcb69d6066765ecd992207515557342
Gefen <gbaranes@mit.edu>
2024-06-14 18:31:31 -0400

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BiasedErasure',
-    version='0.1.3',
+    version='0.1.4',
     author='Gefen Baranes and Pablo Bonilla',
     author_email='gefenbaranes123@gmail.com',
     packages=find_packages(include=['BiasedErasure', 'BiasedErasure.*']),
