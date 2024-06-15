c02f6ddb63f69867195eccbe86bb1faae82b4b8e
Gefen <gbaranes@mit.edu>
2024-06-15 09:15:15 -0400

    cancel_debugging

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BiasedErasure',
-    version='0.1.4',
+    version='0.1.5',
     author='Gefen Baranes and Pablo Bonilla',
     author_email='gefenbaranes123@gmail.com',
     packages=find_packages(include=['BiasedErasure', 'BiasedErasure.*']),
